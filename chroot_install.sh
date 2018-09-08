7ca8d4cb8c6a089c263e6788efbd5f7492a8dc62
leonan789 <nanweijia789@163.com>
2018-09-08 15:04:29 +0800

    Update chroot_install.sh

--- chroot_install.sh
+++ chroot_install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # *****************************************************************************
-# Function:     SDcard fabrication
-# Description:  SDcard format, extract ubuntu filesystem from iso, and copy
-#               (3559/mini).run file into "/var", "/" directory in SDcard.
-# Date:         2018/09/03-v0.5
+# Function:     Execute in chroot
+# Description:  Install ssh and adduser hisilicon
+# Date:         2018/09/08
 # Author:       nanweijia
-# Input:        1.SDcard fullname(e.g:/dev/mmcblk1)
-#               2.Path include ubuntu-xxx-server-arm64.iso and (3559/mini).run
-#               3.iso filename(e.g:ubuntu-16.04.3-server-arm64.iso)
-#               4.mini filename(e.g:xxx.run)
-#               5.3559 filename(e.g:xxx.run)
 # *****************************************************************************
 
 apt-get update
 apt-get install openssh-server -y
+apt-get install unzip -y
+
+adduser --disabled-password --gecos "" hisilicon
+echo -e "hisilicon\nhisilicon" | passwd hisilicon
+echo "hisilicon       ALL=(ALL:ALL) ALL" >> /etc/sudoers
 exit
 # end
