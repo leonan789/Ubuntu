b76d9548d408c1fc2f54d246e6cae451a8f8eb31
leonan789 <nanweijia789@163.com>
2018-09-08 15:05:31 +0800

    Update chroot_install.sh

--- chroot_install.sh
+++ chroot_install.sh
@@ -3,7 +3,7 @@
 # Function:     Execute in chroot
 # Description:  Install ssh and adduser hisilicon
 # Date:         2018/09/08
-# Author:       nanweijia
+# Author:       leonan
 # *****************************************************************************
 
 apt-get update
@@ -11,7 +11,7 @@ apt-get install openssh-server -y
 apt-get install unzip -y
 
 adduser --disabled-password --gecos "" hisilicon
-echo -e "hisilicon\nhisilicon" | passwd hisilicon
-echo "hisilicon       ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo -e "user\nuser" | passwd hisilicon
+echo "user       ALL=(ALL:ALL) ALL" >> /etc/sudoers
 exit
 # end
