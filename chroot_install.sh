f94a846a5d24dca6ee6a95ab5a9cafbb5c18a7cf
leonan789 <nanweijia789@163.com>
2018-09-19 15:17:14 +0800

    Update chroot_install.sh

--- chroot_install.sh
+++ chroot_install.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 # *****************************************************************************
 # Function:     Execute in chroot
-# Description:  Install ssh and adduser hisilicon
-# Date:         2018/09/08
+# Description:  Install software and adduser xxx
+# Date:         2018/09/18_release1.18
 # Author:       leonan
 # *****************************************************************************
 
+locale-gen zh_CN.UTF-8 en_US.UTF-8 en_HK.UTF-8
 apt-get update
 apt-get install openssh-server -y
 apt-get install unzip -y
+apt-get install vim -y
+apt-get install gcc -y
+apt-get install zlib -y
+apt-get install python2.7 -y
+apt-get install python3 -y
+apt-get install pciutils -y
+apt-get install strace -y
+apt-get install nfs-common -y
 
-adduser --disabled-password --gecos "" hisilicon
-echo -e "user\nuser" | passwd hisilicon
-echo "user       ALL=(ALL:ALL) ALL" >> /etc/sudoers
+useradd -m xxx
+sed -i '/^xxx:/c\hisilicon:$6$Zlr1mw83$vw/cmjEg8vpvWj3Cenp9a78lmmx.b7UH2xVvTw32KWwLpwWGRMFb9Fiyw/DoxvaxWIQdz7CkUGjx/cujDkuQR/:17775:0:99999:7:::' /etc/shadow
+echo "xxx       ALL=(ALL:ALL) ALL" >> /etc/sudoers
 exit
 # end
